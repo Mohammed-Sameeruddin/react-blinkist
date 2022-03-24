d65c8f8aa90a97edf16b29322a8cd225223ba317
Mohammed-Sameeruddin <65489551+Mohammed-Sameeruddin@users.noreply.github.com>
2022-03-24 11:19:24 +0530

    Update Logo.stories.tsx

--- src/components/atoms/Logo/Logo.stories.tsx
+++ src/components/atoms/Logo/Logo.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Logo from "./Logo";
 
 export default {
-  title: "Logo",
+  title: "Atoms/Logo",
   component: Logo,
 } as ComponentMeta<typeof Logo>;
 
